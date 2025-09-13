907ade8745467ad1e386df83a0dc9cbe03f98aec
guduri0428 <guduri0428@gmail.com>
2025-09-13 11:17:59 +0530

    student example w.r.t constructor

--- constructor_concept/handson_programmes.py
+++ constructor_concept/handson_programmes.py
@@ -11,3 +11,19 @@ t1 = TestConstructor()
 t2 = TestConstructor()
 t3 = TestConstructor()
 t1.m1()
+
+# One more Example w.r.t the Student class
+
+class Student:
+    def __init__(self,name,rollno,marks):
+        self.name = name
+        self.rollno  = rollno
+        self.marks = marks
+    
+    def display(self):
+        print("Student Name : {}\nRollno : {}\nMarks : {}".format(self.name,self.rollno,self.marks))
+
+s1 = Student("Srinivasulu Guduri",542,82.9)
+s1.display()
+s2 = Student("Guduri Srinivasulu",542,82.9)
+s2.display()
