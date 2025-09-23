897491a27d5d893933a938fbd665c52239fb295c
guduri0428 <guduri0428@gmail.com>
2025-09-23 09:34:37 +0530

    static variable declaration

--- static_variables/declaration.py
+++ static_variables/declaration.py
@@ -7,3 +7,25 @@
 # We we access static variables either by using class name or by using object reference. But recommended 
 # to use the class name.
 
+# Note -1
+# In case of instance variables for every object a separate copy will be created, But in case of
+# static variables for total class only one copy will be created and shaerd by every object of that class.
+
+# Example programme
+
+class Test:
+    x = 10
+    def __init__(self):
+        self.y = 20
+
+t1 = Test()
+t2 = Test()
+
+print(t1.x,t1.y)
+print(t2.x,t2.y)
+
+Test.x = 888
+t1.y = 999
+
+print(t1.x,t1.y)
+print(t2.x,t2.y)
